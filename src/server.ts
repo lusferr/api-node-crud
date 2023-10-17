980aa5a08a7f867c08fb9cc8b6f9d27ee6d56c95
luis <luis@stairsdigital.com>
2023-10-17 19:09:03 -0300

    css swagger

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { AppDataSource } from "./database/data-source";
 import routers from "./app/routes/routes";
 import swaggerUi from "swagger-ui-express";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css"
 
 const swaggerDocument = require('../swagger.json');
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3333
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
 
 app.use(routers);
 
